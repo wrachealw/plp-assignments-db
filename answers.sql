0740d9f2f3df4a413fd5158b21d566c95dab8b2f
wrachealw <wrachealw@gmail.com>
2024-09-14 12:27:19 +0300

    Assignment 2

--- answers.sql
+++ answers.sql
@@ -67,3 +67,79 @@ CREATE TABLE discharges(
 	discharge_date DATE NOT NULL,
     discharge_disposition	VARCHAR(255) NOT NULL
 );
+
+
+-- USE hospital_db;
+
+-- Quiz 1.1 retrieve the first_name, last_name and date_of_birth of all patients.
+-- SELECT first_name, last_name, date_of_birth
+-- FROM patients;
+
+-- Quiz 1.2 retrieve the provider_id, first_name and provider_specialty from the providers table.
+-- SELECT provider_id , first_name , provider_specialty
+-- FROM providers;
+
+-- Quiz 2.1 retrieve all patients whose first names start with "Ab"(wildcard).
+-- SELECT *
+-- FROM patients
+-- WHERE first_name LIKE 'Ab%';
+
+-- Quiz 2.2 retrieve all providers whose specialties end with the letter "y".
+-- SELECT *
+-- FROM providers
+-- WHERE provider_specialty LIKE '%y';
+
+-- Quiz 3.1 all patients born after 1st January 1980.
+-- SELECT *
+-- FROM patients
+-- WHERE date_of_birth > '1980-01-01';
+
+-- Quiz 3.2 retrieve visits where the acuity level is 2 or higher.
+-- SELECT *
+-- FROM ed_visits
+-- WHERE acuity >= '2';
+
+-- Quiz 4.1 find patients who speak Spanish.
+-- SELECT first_name, last_name
+-- FROM patients
+-- WHERE language LIKE 'spanish'
+
+-- Quiz 4.2 retrieve visits where the reason is "Migraine" and the disposition is "Admitted".
+-- SELECT *
+-- FROM ed_visits
+-- WHERE reason_for_visit like 'migraine' 
+	-- AND ed_disposition LIKE 'admited';
+
+-- Quiz 4.3 find patients born between 1975 and 1980.
+-- SELECT *
+-- FROM patients
+-- WHERE date_of_birth BETWEEN '1975-01-01' AND '1980-01-01';
+
+-- Quiz 5.1 retrieve patient names and sort them in alphabetical order by last name.
+-- SELECT last_name, first_name
+-- FROM patients
+-- ORDER BY last_name ASC;
+
+-- Quiz 5.2  list all visits sorted by the date of the visit, starting from the most recent.
+-- SELECT *
+-- FROM visits
+-- ORDER BY date_of_visit DESC;
+
+-- Quiz 6.1 retrieve all admissions where the primary diagnosis is "Stroke" and the discharge disposition is "Home".
+-- SELECT *
+-- FROM admissions, discharges
+-- WHERE primary_diagnosis LIKE 'stroke'
+	-- AND discharge_disposition LIKE 'home';
+    
+-- Quiz 6.2 find providers who joined after 1995 and specialize in either Pediatrics or Cardiology.
+-- SELECT*
+-- FROM providers
+-- WHERE date_joined > '1995-01-01' 
+	-- AND provider_specialty IN ('cardiology' , 'pediatrics');
+
+-- Quiz Bonus Challenge (optional)
+-- lists all discharges where the patient was discharged home and the discharge date is within the first week of March 2018.
+SELECT *
+FROM discharges
+WHERE discharge_disposition like 'home'
+	 AND discharge_date BETWEEN '2018-03-01' AND '2018-03-07';
